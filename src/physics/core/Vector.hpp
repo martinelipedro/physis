a0fd44d55e7af7ccf0e37d2ff948d99baac8375e
martinelipedro <martinelipedro@gmail.com>
2023-09-05 19:00:24 -0300

    refactor: Vector3: struct -> class

--- src/physics/core/Vector.hpp
+++ src/physics/core/Vector.hpp
@@ -1,12 +1,14 @@
 #ifndef PHYSIS_PHYSICS_CORE_VECTOR_HPP
 #define PHYSIS_PHYSICS_CORE_VECTOR_HPP
 
-struct Vector3
+class Vector3
 {
 public:
     float x, y, z;
 private:
     float pad;
+public:
+
 };
 
 #endif
