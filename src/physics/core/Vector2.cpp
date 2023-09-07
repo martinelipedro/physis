c57f52192f2c6450a2e5cf8efe302a352e3c5f53
martinelipedro <martinelipedro@gmail.com>
2023-09-07 14:25:08 -0300

    feat: add Vector2

---
+++ src/physics/core/Vector2.cpp
@@ -0,0 +1,5 @@
+#include "Vector2.hpp"
+
+Vector2::Vector2() : x(0), y(0) {}
+
+Vector2::Vector2(const float x, const float y) : x(x), y(y) {}
---
+++ src/physics/core/Vector2.hpp
@@ -0,0 +1,13 @@
+#ifndef PHYSIS_PHYSICS_CORE_VECTOR2_HPP
+#define PHYSIS_PHYSICS_CORE_VECTOR2_HPP
+
+class Vector2
+{
+public:
+    float x, y;
+public:
+    Vector2();
+    Vector2(const float x, const float y);
+};
+
+#endif
